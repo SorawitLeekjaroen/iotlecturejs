8360bd7120bb1dc261eae167321ae350bf4eb098
SorawitLeekjaroen <springtrap2009@gmail.com>
2025-02-08 20:38:58 +0700

    เรียน JavaScript ครั้งที่ 1

---
+++ js/t02-style.js
@@ -0,0 +1,1 @@
+alert('Wow Wow Wow')
---
+++ nodekm/km01.js
@@ -0,0 +1,26 @@
+
+//String
+console.log('Hello...')
+console.log('Hello...')
+//Alt+9+6 `
+//Alt+1+2+6 ~
+console.log(`Hello`+111+(10+20+true))
+console.log(`Hello${111}${10+20}${true}`)
+
+//Number
+console.log(234)
+console.log(234.23424)
+
+//Boolean
+console.log(true)
+console.log(false)
+
+//Array
+console.log([1,2,3,4,5])
+console.log(['WOW','Hello','Bye'])
+
+//Object
+console.log({name:'John',age:20})
+
+//Expression
+console.log(10+20)
